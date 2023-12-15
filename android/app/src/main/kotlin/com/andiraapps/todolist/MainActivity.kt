514fe243e5c5f35dfbe17c494ecac480a0ffde0d
Mushafa <mushafaandira21@gmail.com>
2023-12-15 23:19:08 +0700

    awal

---
+++ android/app/src/main/kotlin/com/andiraapps/todolist/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.andiraapps.todolist
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/model/todo.dart
@@ -0,0 +1,28 @@
+class Todo {
+  final String uid;
+  final String title;
+  final String description;
+  final bool isComplete;
+
+  Todo({
+    required this.uid,
+    required this.title,
+    required this.description,
+    required this.isComplete,
+  });
+}
+
+// List<Todo> listdata = [
+//   Todo(
+//     nama: 'Studi Kasus 1',
+//     deskripsi: 'Membuat Program Dasar Java',
+//   ),
+//   Todo(
+//     nama: 'Studi Kasus 2',
+//     deskripsi: 'Membuat Studi Kasus List Makanan',
+//   ),
+//   Todo(
+//     nama: 'Studi Kasus 3',
+//     deskripsi: 'Membuat Aplikasi To Do List',
+//   ),
+// ];
